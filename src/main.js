907067c06415c696392e9ca5673830237bd39b4c
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2025-12-10 17:34:32 +0000

    Working 3d enviroment

--- src/main.js
+++ src/main.js
@@ -1,24 +1,57 @@
+// ? Imports
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+  
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+camera.position.setZ(30);
+camera.lookAt(0, 0, 0);
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xffffff,
+  transmission: 1,   // Fully transmissive
+  thickness: `1`,    // Glass thickness
+  roughness: 0.0,      // Smooth surface
+  ior: 1.5,          // Index of refraction for glass
+  attenuationDistance: 2,
+  attenuationColor: 0xffffff,
+});
 
-setupCounter(document.querySelector('#counter'))
+const loader = new GLTFLoader();
+loader.load( 'public/yaka_model.glb', function ( gltf ) {
+  const model = gltf.scene;
+ model.position.set(0, 0, 20);
+  model.rotateY((Math.PI / 2)); 
+  model.traverse( ( child ) => {
+    if ( child.isMesh && child.name === 'path378' ) {
+      console.log(child.name);
+      child.material = glassMaterial;
+    }
+  } );
+  model.scale.set(25, 25, 25);
+  scene.add( model );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
+
+const light = new THREE.AmbientLight(0xffffff);
+const controls = new OrbitControls(camera, renderer.domElement);
+scene.add(light);
+scene.background = new THREE.Color(0xfffafa);
+function animate(){
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera);
+  controls.update();
+}
+animate();
