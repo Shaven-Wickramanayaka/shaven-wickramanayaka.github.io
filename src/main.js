5b6f0d4ad7af2ebd0d9c1b602d0d149dcc36da07
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2025-12-25 10:21:01 +0000

    Fixed relative import url

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import "./style.css";
 // import * as THREE from "three";
 import * as THREE from "../node_modules/three/build/three.module.js";
-import { GLTFLoader } from "../node_modules/three/addons/loaders/GLTFLoader.js";
+import { GLTFLoader } from "../node_modules/three/examples/jsm/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 // Camera parameters
